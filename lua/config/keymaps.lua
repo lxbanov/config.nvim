f340c248e093be8e4c259d09e33b556c0c4b692c
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2026-09-12 12:33:54 +0500

    upd: add surround and fix telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,7 +22,6 @@ map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
 -- Buffers
 map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Prev buffer" })
 map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
-map("n", "<leader><leader>", "<cmd>Telescope buffers<cr>", { desc = "Switch buffer" })
 
 -- Move selected lines
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader><leader>", "<cmd>Telescope buffers<cr>", desc = "Switch buffer" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
         { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
         { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
